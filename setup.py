2227ae31aa9d3ec7b41f48f1e26f46bee06a6fcb
Antonio Piccolboni <antonio@piccolboni.info>
2018-04-25 23:07:44 -0700

    A pkg needs a setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='autosig',
+    version='0.1.0',
+    packages=['.'],
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+    long_description=open('README.md').read(),
+)
