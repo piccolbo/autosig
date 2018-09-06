774420c3ad67fe7780ddc2e01042ff178a4fe2f2
Antonio Piccolboni <antonio@piccolboni.info>
2018-09-05 17:02:59 -0700

    two extensions and formatting

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,12 @@ import autosig
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -78,7 +83,6 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-
 # -- Options for HTML output -------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -97,12 +101,10 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'autosigdoc'
-
 
 # -- Options for LaTeX output ------------------------------------------
 
@@ -128,22 +130,20 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'autosig.tex',
-     u'autosig Documentation',
-     u'Antonio Piccolboni', 'manual'),
+    (
+        master_doc,
+        'autosig.tex',
+        u'autosig Documentation',
+        u'Antonio Piccolboni',
+        'manual',
+    ),
 ]
-
 
 # -- Options for manual page output ------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'autosig',
-     u'autosig Documentation',
-     [author], 1)
-]
-
+man_pages = [(master_doc, 'autosig', u'autosig Documentation', [author], 1)]
 
 # -- Options for Texinfo output ----------------------------------------
 
@@ -151,13 +151,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'autosig',
-     u'autosig Documentation',
-     author,
-     'autosig',
-     'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        'autosig',
+        u'autosig Documentation',
+        author,
+        'autosig',
+        'One line description of project.',
+        'Miscellaneous',
+    ),
 ]
-
-
-
