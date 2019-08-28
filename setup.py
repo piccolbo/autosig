2e970b5dd3e41b4be7580985db8d67fb2638f9c9
Antonio Piccolboni <antonio@piccolboni.info>
2019-08-27 22:47:51 -0700

    Bump version: 0.7.0 → 0.8.0

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/piccolbo/autosig',
-    version='0.7.0',
+    version='0.8.0',
     zip_safe=False,
 )
