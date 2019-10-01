35cc3613eafff7bde3efcf08c9576cf1659eb8b4
Antonio Piccolboni <antonio@piccolboni.info>
2019-10-01 08:38:39 -0700

    Bump version: 0.8.2 → 0.9.0

--- autosig/__init__.py
+++ autosig/__init__.py
@@ -5,4 +5,4 @@ from .autosig import Signature, autosig, param
 __all__ = ["Signature", "autosig", "param"]
 __author__ = """Antonio Piccolboni"""
 __email__ = "autosig@piccolboni.info"
-__version__ = "0.8.2"
+__version__ = "__version__ = '0.9.0'"
