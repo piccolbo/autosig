8ca093039ae0440c013598845cdafb701b91701a
Antonio Piccolboni <antonio@piccolboni.info>
2020-07-01 14:26:21 -0700

    Bump version: 0.9.2 → 0.10.0

--- autosig/__init__.py
+++ autosig/__init__.py
@@ -5,4 +5,4 @@ from .autosig import Signature, autosig, param, Retval
 __all__ = ["Signature", "autosig", "param", "Retval"]
 __author__ = """Antonio Piccolboni"""
 __email__ = "autosig@piccolboni.info"
-__version__ = "__version__ = '0.9.2'"
+__version__ = "__version__ = '0.10.0'"
